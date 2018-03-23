924918a458219f5ef61c5d0578f2d4e206cdc563
Lilith Whittles <lilithwhittles@gmail.com>
2018-03-23 12:19:17 +0000

    tidying

--- calc_dd.R
+++ calc_dd.R
@@ -1,8 +1,15 @@
 calc_dd <- function(degree_vec, N) {
+  
   dd <- rep(0, max(degree_vec))
+  
   degree_table <- table(degree_vec)
-  dd[as.numeric(names(degree_table)[-1])] <- degree_table[-1]
+  
   prop0 <- degree_table[1]/N
+  
+  degree_table <- degree_table[-1]
+  dd[as.numeric(names(degree_table))] <- degree_table
+
   dd <- dd/sum(dd)
+  
   return(list(prop0 = prop0, dd=dd))
 }
--- calc_g.R
+++ calc_g.R
@@ -5,10 +5,8 @@ calculate_g <- function(x, gamma, k0, N, kmax = N - 1, mu = 1) {
   k <- function(x) { # k in terms of F(lambda)
     ((1-gamma)/const*x + k0^(1-gamma)) ^ (1/(1-gamma)) 
   }
+  
   kdash <- integrate(f = k, lower = 0, upper = 1)
-  # check_kdash <- ((const*k0^(1-gamma) + 1-gamma) * (k0^(1-gamma) + (1-gamma)/const) ^ (1/(1-gamma)) - const*k0^(2-gamma)) / (2-gamma)
-  # print(kdash$value); print(check_kdash)
-  # working fine, so go with simpler option in terms of code
   
   gbar <- sqrt(kdash$value / (N-1))
   Fl <- pexp(x, rate = mu) # since lambdas are dist Exp(mu)
